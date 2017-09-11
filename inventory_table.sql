fdd32eb85ce529411072e07385ee752c5cfa33ec
jeffroliv <jeffroliv@hotmail.com>
2017-09-11 16:34:35 -0700

    Update inventory_table.sql
    WORK IN PROGRESS

--- inventory_table.sql
+++ inventory_table.sql
@@ -1,6 +1,6 @@
 create table inventory (
 id TINYINT(5) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(255) NOT NULL,
+product_name VARCHAR(255) NOT NULL,
 description TEXT NOT NULL,
 price INT(5) UNSIGNED NOT NULL,
 quantity TINYINT(3) UNSIGNED NOT NULL,
@@ -8,4 +8,52 @@ heat_rate TINYINT(1) UNSIGNED NOT NULL,
 cat_id TINYINT(3) UNSIGNED NOT NULL,
 image VARCHAR(255) NULL,
 review LONGTEXT NULL
+);
+
+create table category (
+  pepper VARCHAR(20) NOT NULL,
+  sauces VARCHAR(20) NOT NULL
+);
+
+create table heat_rating (
+  mild VARCHAR(20) NOT NULL,
+  medium VARCHAR(20) NOT NULL,
+  hot VARCHAR(20) NOT NULL,
+  fire VARCHAR(20) NOT NULL,
+  volcano VARCHAR(20) NOT NULL,
+  diablo VARCHAR(20) NOT NULL
+);
+
+create table account (
+  acc_id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  password VARCHAR(20) NOT NULL,
+  addr_num INT(10) NOT NULL,
+  addr_street VARCHAR(20) NOT NULL 
+  #transaction history reference
+  #cart reference
+);
+
+create table transaction_history (
+  id_num INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  prod_name VARCHAR(255) NOT NULL,
+  price DECIMAL(10,2) NOT NULL DEFAULT 0,
+  quantity INT(300) NULL,
+  heat_rate VARCHAR(20) NOT NULL, #reference to heat rating
+  category VARCHAR(20) NOT NULL, #reference to category 
+  trans_image VARCHAR(255) NULL
+);
+
+create table cart (
+  id_num_cart INT(100) NOT NULL PRIMARY KEY, #reference product id 
+  acc_id_cart INT(100) NOT NULL PRIMARY KEY, #references account
+  prod_name_cart VARCHAR(255) NOT NULL,
+  price_cart DECIMAL(10,2) NOT NULL DEFAULT 0,
+  quantity_cart INT(300) NULL,
+  heat_rate_cart VARCHAR(20) NOT NULL,
+  category_cart VARCHAR(20) NOT NULL,
+  image_cart VARCHAR(255) NULL,
+  sub_total DECIMAL(10,2) NOT NULL DEFAULT 0,
+  total DECIMAL(10,2) NOT NULL DEFAULT 0
 );
