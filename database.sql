18618bdc13d323963d4f3ec72cf78335ccf99d7b
jeffroliv <jeffroliv@hotmail.com>
2017-09-18 15:41:24 -0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE `jalapeno`;
 
-create table inventory (
+CREATE TABLE INVENTORY (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   product_name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
@@ -12,19 +12,19 @@ create table inventory (
   review LONGTEXT NULL
 );
 
-create table category (
+CREATE TABLE CATEGORY (
   cate_id INT NOT NULL,
   category VARCHAR(20) NOT NULL,
-  FOREIGN KEY (cate_id) REFERENCES inventory(cat_id)
+  FOREIGN KEY (cate_id) REFERENCES INVENTORY(cat_id)
 );
 
-create table heat_rating (
+CREATE TABLE HEAT_RATING (
   heat_id INT NOT NULL,
   heat VARCHAR(20) NOT NULL,
-  FOREIGN KEY (heat_id) REFERENCES inventory(heat_rate)
+  FOREIGN KEY (heat_id) REFERENCES INVENTORY(heat_rate)
 );
 
-create table account (
+CREATE TABLE ACCOUNT (
   acc_id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -33,22 +33,22 @@ create table account (
   addr_street VARCHAR(20) NOT NULL 
 );
 
-create table transaction_history (
+CREATE TABLE TRANSACTION_HISTORY (
   id_num_t INT(100) NOT NULL AUTO_INCREMENT,
   prod_name_t VARCHAR(255) NOT NULL,
   price_t DECIMAL(10,2) NOT NULL DEFAULT 0,
   heat_rate_t INT NOT NULL, #reference to heat rating
   category_t INT NOT NULL, #reference to category 
   image_t VARCHAR(255) NULL,
-  FOREIGN KEY (id_num) REFERENCES inventory(id),
-  FOREIGN KEY (prod_name_t) REFERENCES inventory(product_name),
-  FOREIGN KEY (price_t) REFERENCES inventory(price),
-  FOREIGN KEY (image_t) REFERENCES inventory(image),
-  FOREIGN KEY (heat_rate_t) REFERENCES heat_rating(heat_id),
-  FOREIGN KEY (category_t) REFERENCES category(cate_id)
+  FOREIGN KEY (id_num) REFERENCES INVENTORY(id),
+  FOREIGN KEY (prod_name_t) REFERENCES INVENTORY(product_name),
+  FOREIGN KEY (price_t) REFERENCES INVENTORY(price),
+  FOREIGN KEY (image_t) REFERENCES INVENTORY(image),
+  FOREIGN KEY (heat_rate_t) REFERENCES HEAT_RATING(heat_id),
+  FOREIGN KEY (category_t) REFERENCES CATEGORY(cate_id)
 );
 
-create table cart (
+CREATE TABLE CART (
   cart_id INT(100) NOT NULL
   id_num_cart INT(100) NOT NULL, #reference product id 
   acc_id_cart INT(100) NOT NULL, #references account
@@ -60,13 +60,13 @@ create table cart (
   image_cart VARCHAR(255) NULL,
   sub_total DECIMAL(10,2) DEFAULT 0,
   total DECIMAL(10,2) DEFAULT 0,
-  FOREIGN KEY (id_num_cart) REFERENCES inventory(id),
-  FOREIGN KEY (acc_id_cart) REFERENCES account(acc_id),
-  FOREIGN KEY (prod_name_cart) REFERENCES inventory(product_name),
-  FOREIGN KEY (price_cart) REFERENCES inventory(price),
-  FOREIGN KEY (quantity_cart) REFERENCES inventory(quantity),
-  FOREIGN KEY (heat_rate_cart) REFERENCES heat_rating(heat_id),
-  FOREIGN KEY (category_cart) REFERENCES category(cate_id),
-  FOREIGN KEY (image_cart) REFERENCES inventory(image),
-  FOREIGN KEY (cart_id) REFERENCES account(acc_id)
+  FOREIGN KEY (id_num_cart) REFERENCES INVENTORY(id),
+  FOREIGN KEY (acc_id_cart) REFERENCES ACCOUNT(acc_id),
+  FOREIGN KEY (prod_name_cart) REFERENCES INVENTORY(product_name),
+  FOREIGN KEY (price_cart) REFERENCES INVENTORY(price),
+  FOREIGN KEY (quantity_cart) REFERENCES INVENTORY(quantity),
+  FOREIGN KEY (heat_rate_cart) REFERENCES HEAT_RATING(heat_id),
+  FOREIGN KEY (category_cart) REFERENCES CATEGORY(cate_id),
+  FOREIGN KEY (image_cart) REFERENCES INVENTORY(image),
+  FOREIGN KEY (cart_id) REFERENCES ACCOUNT(acc_id)
 );
