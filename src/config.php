79dd6d3b2f782f0f0f76defc6743291b20bcb27b
Mark Yang <mark.h.yang@gmail.com>
2017-09-21 03:31:57 -0700

    Reorganize files. Added basic page routing.

---
+++ src/config.php
@@ -0,0 +1,25 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "hoanghiepp";
+$dbname = "jalapeno";
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error . " " . $conn->connect_errno);
+    }else{
+        //echo "Connected successfully<br />";
+    }
+
+    $conn->select_db($dbname);
+    // Select database
+    if($conn->error){
+        die("Cannot select database " . $conn->error);
+    }else{
+        //echo "Database selected";
+    }
+
+?>
