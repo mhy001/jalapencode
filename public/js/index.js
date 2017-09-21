bc4739ece7b046a56c46829e0728ec4159f89440
Mark Yang <mark.h.yang@gmail.com>
2017-09-21 16:58:41 -0700

    Add individual product link to product cards. Add dummy product php. Add navbar placeholders.

--- public/js/index.js
+++ public/js/index.js
@@ -6,16 +6,22 @@ $.getJSON("products", function(data) {
     for (var key in data.items) {
       var item = data.items[key];
       var card = "<div class='card rounded expand'>"
-                + "<img class='card-img-top' src='" + item.url + "' alt='" + item.name + "'>"
-                  + "<div class='card-body' title='" + item.name + "'>"
+                  + "<a class='pointer_hand' href='product?id=" + item.id + "'>"
+                    + "<img class='card-img-top' src='" + item.imageURL + "' alt='" + item.name + "'>"
                     + "<h4 class='card-title'>" + item.name + "</h4>"
-                    + "<p class='card-text'>" + item.description + "</p>"
-                    + "<div class=''>"
-                      + "<span class='item-rating'>" + item.heatRating + "</span>"
+                  + "</a>"
+                  + "<div class='card-body'>"
+                    + "<p class=''>" + item.description + "</p>"
+                    + "<div>"
+                      + "<span class='item_heatRating'>" + item.heatRating + "</span>"
                       + "<span class='text-danger float-right'>$" + item.price + "</span>"
                     + "</div>"
                   + "</div>"
-                  + "<button class='btn'>Action</button>"
+                  + "<button class='btn btn-light'>Add to cart</button>"
+                  + "<div class='d-none'>"
+                    + "<span class='item_quantity'>" + item.quantity + "</span>"
+                    + "<span class='item_reviewRating'>" + item.reviewRating + "</span>"
+                  + "</div>"
                 + "</div>";
       $(".product-grid").append(card);
     }
