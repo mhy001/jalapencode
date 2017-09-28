a9ba99a1c12fb1a662d50733bd9accd0c6eec284
Mark Yang <mark.h.yang@gmail.com>
2017-09-28 00:21:51 -0700

    Add fire icon for heat, icon to navbar, details to about page

--- public/js/app.js
+++ public/js/app.js
@@ -182,15 +182,22 @@ function showCartButton() {
  * INDEX.HTML
  */
 function populateProductGrid(product) {
+  var heat = "";
+  for (i = 0; i < product.heat; i++) {
+    heat += "<svg class='fire' height='20' viewBox='0 0 24 24' width='20' xmlns='http://www.w3.org/2000/svg'>"
+            + "<path d='M13.5.67s.74 2.65.74 4.8c0 2.06-1.35 3.73-3.41 3.73-2.07 0-3.63-1.67-3.63-3.73l.03-.36C5.21 7.51 4 10.62 4 14c0 4.42 3.58 8 8 8s8-3.58 8-8C20 8.61 17.41 3.8 13.5.67zM11.71 19c-1.78 0-3.22-1.4-3.22-3.14 0-1.62 1.05-2.76 2.81-3.12 1.77-.36 3.6-1.21 4.62-2.58.39 1.29.59 2.65.59 4.04 0 2.65-2.15 4.8-4.8 4.8z'/>"
+            + "<path d='M0 0h24v24H0z' fill='none'/>"
+          + "</svg>";
+  }
   var card = "<div id='" + product.id + "' class='card rounded expand'>"
               + "<a class='pointer-hand' href='product?" + product.id + "'>"
                 + "<img class='card-img-top' src='" + product.url + "' alt='" + product.name + "'>"
                 + "<h4 class='card-title ml-3'>" + product.name + "</h4>"
               + "</a>"
               + "<div class='card-body'>"
-                + "<p class=''>" + product.id + " " + product.description + "</p>"
+                + "<p class=''>" + product.description + "</p>"
                 + "<div>"
-                  + "<span class=''>" + product.heat + "</span>"
+                  + "<span class=''>" + heat + "</span>"
                   + "<span class='text-danger float-right'>$" + product.price + "</span>"
                 + "</div>"
               + "</div>"
