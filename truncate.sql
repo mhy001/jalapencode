80ad0006b08938160010b9e62ca747a812f90811
jeffroliv <jeffroliv@hotmail.com>
2017-10-02 15:37:27 -0700

    Update truncate.sql

--- truncate.sql
+++ truncate.sql
@@ -1,3 +1,3 @@
-mysql -u root --password=PASSWORD -Nse 'show tables' jalepencode | while read table;
+mysql -u root --password=PASSWORD -Nse 'show tables' jalapencode | while read table;
 do mysql -u root --password=PASSWORD -e "set_foreign_key_checks=0;
-truncate table $table" jalepencode; done
+truncate table $table" jalapencode; done
