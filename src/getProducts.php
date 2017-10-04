130de10f5dfdc8e91afbf675755813a4f227e66a
Fierid Neil <14002607+FieridNeil@users.noreply.github.com>
2017-10-04 16:32:24 -0700

    json

--- src/getProducts.php
+++ src/getProducts.php
@@ -2,8 +2,36 @@
 /* Purpose: Return JSON of all products */
 
 header('Content-type: application/json');
+
+require_once("../data/config.php");
+
+$conn->select_db('jalapeno');
+
+
+// RETRIEVE DATA FROM INVENTORY TABLE
+$sql = "SELECT * FROM INVENTORY";
+
+$result = $conn->query($sql);
+
 $items = array();
 
+if(!$result){
+    echo "Query fail" .  $conn->error . "<br />";
+}
+
+while($row = $result->fetch_assoc()){
+    $item = new stdClass();
+
+    $item = (object)$row;
+    
+    $items[] = $item;
+    
+
+}
+
+
+
+/*
 for ($x = 0; $x < 20; $x++) {
   $item = new StdClass;
   $item->id = $x;
@@ -17,7 +45,7 @@ for ($x = 0; $x < 20; $x++) {
   $item->category = 0;
   $items[] = $item;
 }
-
+*/
 echo json_encode($items);
 
 ?>
