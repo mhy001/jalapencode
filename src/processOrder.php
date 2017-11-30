8271cdfea3ee6dc160038820ea7f210dcdeed09e
Fierid Neil <14002607+FieridNeil@users.noreply.github.com>
2017-11-29 16:35:38 -0800

    commit

--- src/processOrder.php
+++ src/processOrder.php
@@ -1,5 +1,8 @@
 <?php
 /* Purpose: Process customer cart */
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 if (empty($_POST["firstName"]) || empty($_POST["lastName"]) || empty($_POST["email"])
     || empty($_POST["address"]) || empty($_POST["city"]) || empty($_POST["state"])
@@ -18,40 +21,73 @@ if (empty($_POST["firstName"]) || empty($_POST["lastName"]) || empty($_POST["ema
   $state = $_POST["state"];
   $zip = $_POST["zip"];
 
-  // TODO: handle real account
-  $sql = "INSERT INTO ACCOUNT (fname, lname, username, password, email, addr, addr_2, addr_city, addr_state, addr_zipcode)
-          VALUES ('{$firstName}', '{$lastName}', 'guest_{$sessionID}', '', '{$email}', '{$address}', '{$address2}', '{$city}', '{$state}', '{$zip}')";
-  $result = $conn->query($sql);
-  if (!$result) {
-    echo $sql . "Query failed" . $conn->error . PHP_EOL;
-  }
-
-  // TODO: handle real account
-  $sql = "SELECT MAX(id) as max FROM ACCOUNT";
-  $result = $conn->query($sql);
-  if (!$result) {
-    echo $sql . "Query failed" . $conn->error . PHP_EOL;
-  } else {
-    $row = $result->fetch_assoc();
-    $customerID = $row["max"];
-
-    $sql = "UPDATE SESSION SET account_id={$customerID} WHERE id='{$sessionID}'";
-    $result = $conn->query($sql);
-
-    if (!$result) {
-      echo $sql . "Query failed" . $conn->error . PHP_EOL;
-    }
-  }
-
-  $sql = "UPDATE CART SET purchased=1 WHERE purchased=0 AND session_id='{$sessionID}'";
-  $result = $conn->query($sql);
-  if (!$result) {
-    echo $sql . "Query failed" . $conn->error . PHP_EOL;
+    if(!isset($_SESSION['username'])){
+      // Ghost/guest account
+      $sql = "INSERT INTO ACCOUNT (fname, lname, username, password, email, addr, addr_2, addr_city, addr_state, addr_zipcode)
+              VALUES ('{$firstName}', '{$lastName}', 'guest_{$sessionID}', '', '{$email}', '{$address}', '{$address2}', '{$city}', '{$state}', '{$zip}')";
+      $result = $conn->query($sql);
+      if (!$result) {
+        echo $sql . "Query failed" . $conn->error . PHP_EOL;
+      }
+      
+      $sql = "SELECT MAX(id) as max FROM ACCOUNT";
+      $result = $conn->query($sql);
+      if (!$result) {
+        echo $sql . "Query failed" . $conn->error . PHP_EOL;
+      } else {
+        $row = $result->fetch_assoc();
+        $customerID = $row["max"];
+    
+        $sql = "UPDATE SESSION SET account_id={$customerID} WHERE id='{$sessionID}'";
+        $result = $conn->query($sql);
+    
+        if (!$result) {
+          echo $sql . "Query failed" . $conn->error . PHP_EOL;
+        }
+      }
+    
+      $sql = "UPDATE CART SET purchased=1 WHERE purchased=0 AND session_id='{$sessionID}'";
+      $result = $conn->query($sql);
+      if (!$result) {
+        echo $sql . "Query failed" . $conn->error . PHP_EOL;
+      }
+  
+  }else{
+    // Real account
+      $sql = "SELECT * FROM account WHERE username = '{$_SESSION['username']}'";
+      $result = $conn->query($sql);
+      if (!$result) {
+        echo $sql . "Query failed" . $conn->error . PHP_EOL;
+      }else{
+        $user_info = $result->fetch_assoc();
+        $user_id = $user_info['id'];
+        
+        $sql = "SELECT * FROM session WHERE account_id = '{$user_id}'";
+        $result = $conn->query($sql);
+        if (!$result) {
+            echo $sql . "Query failed" . $conn->error . PHP_EOL;
+        }else{
+            $session_info = $result->fetch_assoc();
+                $sql = "UPDATE CART SET purchased=1 WHERE purchased=0 AND session_id='{$session_info['id']}'";
+              $result = $conn->query($sql);
+              if (!$result) {
+                echo $sql . "Query failed" . $conn->error . PHP_EOL;
+              }
+        }
+      
+        
+      }
+      
+      $sql = "UPDATE CART SET purchased=1 WHERE purchased=0 AND session_id='{$sessionID}'";
+      $result = $conn->query($sql);
+      if (!$result) {
+        echo $sql . "Query failed" . $conn->error . PHP_EOL;
+      }
   }
 
   // each cart is associated with a unique session
   // an account will be associated with multiple sessions
-  // session_regenerate_id();
+   session_regenerate_id();
 
   require '../public/view/receipt.phtml';
 }
